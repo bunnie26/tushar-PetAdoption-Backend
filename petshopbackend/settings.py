3eb67344b90ed2dd4c63a7b91193612c35e6cfe9
Vinay <Vinay.lakhani@ronianalytics.com>
2023-11-25 18:30:28 +0530

    WIP

--- petshopbackend/settings.py
+++ petshopbackend/settings.py
@@ -89,11 +89,11 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-    
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
     
